b9fa62b43af1af68e8be2cbbf73ea951ddf9de9a
Roman Z <roman-z@inbox.ru>
2025-07-01 10:26:22 +0500

    Make any changes

--- src/product.py
+++ src/product.py
@@ -54,4 +54,6 @@ class Product:
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
 
     def __add__(self, other):
-        return self.__price * self.quantity + other.__price * other.quantity
+        if type(other) is Product:
+            return self.__price * self.quantity + other.__price * other.quantity
+        raise TypeError
