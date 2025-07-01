3cffc71287f6b4bc5e655e31d05078f859797c9a
Roman Z <roman-z@inbox.ru>
2025-07-01 09:27:33 +0500

    Make any changes

--- src/lawngrass.py
+++ src/lawngrass.py
@@ -1,4 +1,19 @@
 from src.product import Product
 
+
 class Lawngrass (Product):
-    pass
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
+
+# if __name__ == "__main__":
+#     grass1 = Lawngrass("LawnGrass1", "Lawngrass - this is lawngrass", 30000.0, 2, "Hungary", 500)
+#     print(grass1)
+#     print(grass1.name)
+#     print(grass1.description)
+#     print(grass1.price)
+#     print(grass1.quantity)
+#     print(grass1.country)
+#     print(grass1.germination_period)
--- src/smartphone.py
+++ src/smartphone.py
@@ -2,15 +2,21 @@ from src.product import Product
 
 
 class Smartphone (Product):
-    def __init__(self, name, description, price, quantity, efficiency):
-        super().__init__(self, name, description, price, quantity)
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
+        super().__init__(name, description, price, quantity)
         self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
 
 
-if __name__ == "__main__":
-    smartphone1 = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 500)
-    print(smartphone1.name)
-    print(smartphone1.description)
-    print(smartphone1.price)
-    print(smartphone1.quantity)
-    print(smartphone1.efficiency)
+# if __name__ == "__main__":
+#     smartphone1 = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 500, "Samsung Galaxy S23 Ultra", "256GB", "Серый цвет")
+#     print(smartphone1.name)
+#     print(smartphone1.description)
+#     print(smartphone1.price)
+#     print(smartphone1.quantity)
+#     print(smartphone1.efficiency)
+#     print(smartphone1.model)
+#     print(smartphone1.memory)
+#     print(smartphone1.color)
