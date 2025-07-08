488462b7764fd2ab5769a89ecb259aa4c1626306
Roman Z <roman-z@inbox.ru>
2025-07-08 14:07:11 +0500

    Homework ready

--- tests/test_print_mixin.py
+++ tests/test_print_mixin.py
@@ -1,6 +1,6 @@
+from src.lawngrass import Lawngrass
 from src.product import Product
 from src.smartphone import Smartphone
-from src.lawngrass import Lawngrass
 
 
 def test_print_mixin(capsys):
@@ -9,15 +9,11 @@ def test_print_mixin(capsys):
     assert message.out.strip() == "Product(Iphone 15, 512GB, Gray space, 210000.0, 8)"
 
     Smartphone(
-        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5,
-        95.5, "S23 Ultra", 256, "Серый"
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5, "S23 Ultra", 256, "Серый"
     )
     message = capsys.readouterr()
     assert message.out.strip() == "Smartphone(Samsung Galaxy S23 Ultra, 256GB, Серый цвет, 200MP камера, 180000.0, 5)"
 
-    Lawngrass(
-        "Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия",
-        "7 дней", "Зеленый"
-              )
+    Lawngrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
     message = capsys.readouterr()
     assert message.out.strip() == "Lawngrass(Газонная трава, Элитная трава для газона, 500.0, 20)"
