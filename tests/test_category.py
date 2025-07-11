fe820521741634cec2cbfc3021ac3249a0ab199e
Roman Z <roman-z@inbox.ru>
2025-07-11 15:08:00 +0500

    Make any changes

--- tests/test_category.py
+++ tests/test_category.py
@@ -29,8 +29,17 @@ def test_exceptions(capsys, first_category):
     add_product = Product("Xiaomi Redmi Note 11","1024GB, Синий", 31000.0, 14)
     first_category.add_product(add_product)
     message = capsys.readouterr()
-    print(message)
     assert message.out.strip().split("\n")[-2] == "Количество товара Xiaomi Redmi Note 11 увеличено на 14 шт."
     assert message.out.strip().split("\n")[-1] == "Обработка товара прошла успешно."
 
 
+def test_null(capsys, first_category):
+    # add_product = Product("Xiaomi Redmi Note 11","1024GB, Синий", 31000.0, 0)
+    # first_category.add_product(add_product)
+    message = capsys.readouterr()
+    print(message)
+    with pytest.raises(ValueError):
+        add_product = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 0)
+        first_category.add_product(add_product)
+    assert message.out.strip().split("\n")[-2] == "Product(Iphone 15, 512GB, Gray space, 210000.0, 8)"
+    assert message.out.strip().split("\n")[-1] == "Product(Xiaomi Redmi Note 11, 1024GB, Синий, 31000.0, 14)"
